5632821a3c72837d73075d0c1942c9368304dfb5
Oleh Smolynets <smolynets@gmail.com>
2020-07-19 14:31:03 +0300

    fix: removed real token, sqlite, real url and env

--- myviberbot/sethook.py
+++ myviberbot/sethook.py
@@ -2,13 +2,13 @@
 import requests
 import json
 
-auth_token = "4bd371ba60e7dc6f-bd2b285c3eb35da7-c73e528a718675ea"
+auth_token = "token"
 hook = "https://chatapi.viber.com/pa/set_webhook"
 headers = {"X-Viber-Auth-Token": auth_token}
 
 
 sen = dict(
-    url="https://2a0d5b265b63.ngrok.io/webhook2020",
+    url="https://custome_url_or_ngrok/webhook2020",
     event_types=["unsubscribed", "conversation_started", "message", "seen", "delivered"],
 )
 r = requests.post(hook, json.dumps(sen), headers=headers)
